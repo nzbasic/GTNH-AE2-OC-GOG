f9c211913b2e4926435a2f3025a2d6fa76b41a1b
James <54062686+nzbasic@users.noreply.github.com>
2024-06-16 11:54:29 +1200

    Nocheck deno

--- supabase/functions/clean/index.ts
+++ supabase/functions/clean/index.ts
@@ -1,3 +1,5 @@
+// @ts-nocheck
+
 // Follow this setup guide to integrate the Deno language server with your editor:
 // https://deno.land/manual/getting_started/setup_your_environment
 // This enables autocomplete, go to definition, etc.
